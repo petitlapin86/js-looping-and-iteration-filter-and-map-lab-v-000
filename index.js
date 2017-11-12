9aa22ec99ec8fcc9dc21550075a9fa387d363e9c
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-12 14:34:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
 function driversWithRevenueOver (speeddemons, revenue) {
-  return speeddemons.filter(function (driver) {
-    return driver.revenue > revenue;
+  return speeddemons.filter(function (poodle) {
+    return poodle.revenue > revenue;
   });
 }
